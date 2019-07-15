f4fa0828c12dd8b0e5e02b78fa5ea330fa1148ed
emiliog-c <troubledshoe22500@gmail.com>
2019-07-15 16:10:46 +1000

    Changed name of dash script, added favicon and fixed pws

--- dash/app_passwords.py
+++ dash/app_passwords.py
@@ -1,7 +1,7 @@
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'admin': 'cot3.hop'
-    'sensor1': 'pug0,pop'
-    'sensor2': 'git6:ace'
-    'sensor3': 'ham1?mum'
+    'admin': 'cot3.hop',
+    'sensor1': 'pug0,pop',
+    'sensor2': 'git6:ace',
+    'sensor3': 'ham1?mum',
     'sensor4': 'MSS3,rev'
 }

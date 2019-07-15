f99df8e8c696a4e327a425fd93e20918a1b2154a
emiliog-c <troubledshoe22500@gmail.com>
2019-07-15 16:04:13 +1000

    updated passwords

--- dash/app_passwords.py
+++ dash/app_passwords.py
@@ -1,3 +1,7 @@
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'admin': 'ooh7-yew'
+    'admin': 'cot3.hop'
+    'sensor1': 'pug0,pop'
+    'sensor2': 'git6:ace'
+    'sensor3': 'ham1?mum'
+    'sensor4': 'MSS3,rev'
 }

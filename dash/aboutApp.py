7ef9951dc24a3a1d1226da6b8a5d620d62e35032
emiliog-c <troubledshoe22500@gmail.com>
2019-07-19 18:00:30 +1000

    Updated About file

--- dash/aboutApp.py
+++ dash/aboutApp.py
@@ -7,16 +7,11 @@ def aboutApp():
             html.H4("About this app", className='card-title'),
             html.H6("Written by Emilio Guevarra Churches", className='card-subtitle'),
             html.P(
-                "Placeholder"
-                "placeholder"
-                "placeholder",
-                className="card-text"),
-            html.Img(src="/assets/Basic-structure-of-a-pyrimidine-ring.png"),
-            html.P(
-                "Placeholder"
-                "placeholder"
-                "placeholder",
-                className="card-text",),
+                "This website shows all the information that is being compiled by multiple Raspberry Pi Sensors in different locations."
+                "The Raspberry Pi's are equipped with Honeywell Particle Sensors (http://www.farnell.com/datasheets/2313714.pdf), capable of measuring PM2.5 and PM10 particles."
+                "It is also equipped with an Adafruit DHT22 temperature sensor (https://learn.adafruit.com/dht) and a BMP180 air pressure sensor (https://tutorials-raspberrypi.com/raspberry-pi-and-i2c-air-pressure-sensor-bmp180/)."
+                "All code regarding the creation of this project is run with Python, and the libraries Dash, plotly, Bootstrap, the sensors respective libraries, Amazon IoT, Pandas Boto3 and DynamoDB.",
+                className="card-text"),       
         ])
     )
                     

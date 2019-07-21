e4ee7a82ce69dc524187a829b2eff51d707ca2df
emiliog-c <51037944+emiliog-c@users.noreply.github.com>
2019-07-21 22:30:28 +1000

    move pictures around in the about page

--- dash/aboutApp.py
+++ dash/aboutApp.py
@@ -48,10 +48,10 @@ def aboutApp():
                 		'''The Raspberry Pis are equipped with [Honeywell Particle Sensors](http://www.farnell.com/datasheets/2313714.pdf), capable of measuring PM2.5 and PM10 particles.
                 		It is also equipped with an [Adafruit DHT22 temperature sensor](https://learn.adafruit.com/dht) and a [BMP180 air pressure sensor](https://tutorials-raspberrypi.com/raspberry-pi-and-i2c-air-pressure-sensor-bmp180/). 
                         More information can be found at the [Github page for this project](https://github.com/emiliog-c/SDD-Sensor-App).''']),
-                sDiagram,
                 html.P(
                 		"All code regarding the creation of this project is written in Python, and the libraries Dash, plotly, dash-Bootstrap, the sensors' respective libraries, Amazon IoT libraries, Pandas, Boto3 and DynamoDB.",
                 		className="card-text"),
+                sDiagram,
             	sensorPhotos,
         		])          
     return a
